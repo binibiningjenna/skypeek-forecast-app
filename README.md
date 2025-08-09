9d5a452ff62e0c92d90b39b5db35c8bbef51279e
Jenna Miles Reyes <atienzajennamiles@gmail.com>
2025-08-09 23:11:59 +0800

    skypeek | api integration

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# skypeek-forecast-app
-SkyPeek is a sleek, responsive weather app that fetches live data from OpenWeatherMap. Features include city/zip search, geolocation support, dynamic visuals, and a beautiful dark/light theme.
+# 🌤️ SkyPeek – Weather Forecast Web App
+
+> **SkyPeek** is a sleek, responsive weather forecast web application that allows users to search weather by city or get real-time weather updates using their **current location**. It provides **today's hourly forecast** and a **5-day forecast** using the OpenWeather API.
+
+![alt text](assets/img/image.png)
+
+---
+
+## ✨ Features
+
+- 🔍 **Search Weather by City** – Get instant weather updates for any location worldwide.
+- 📍 **Current Location Weather** – Uses geolocation to fetch weather data where you are.
+- ⏳ **Today's Hourly Forecast** – Detailed temperature and condition updates hour-by-hour.
+- 📅 **5-Day Forecast** – Plan ahead with upcoming weather predictions.
+- 🌡️ **Temperature, Humidity, Wind Speed & Conditions** – All essential weather metrics.
+- 📱 **Responsive Design** – Works beautifully on desktop and mobile devices.
+
+---
+
+## 🛠️ Tech Stack & Tools
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?logo=html5&logoColor=white&style=for-the-badge)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?logo=css3&logoColor=white&style=for-the-badge)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=black&style=for-the-badge)
+![Bootstrap](https://img.shields.io/badge/Bootstrap-7952B3?logo=bootstrap&logoColor=white&style=for-the-badge)
+![OpenWeather API](https://img.shields.io/badge/OpenWeather%20API-FF6F00?logo=cloud&logoColor=white&style=for-the-badge)
+![amCharts](https://img.shields.io/badge/amCharts-CC0000?logo=chart-bar&logoColor=white&style=for-the-badge)
+
+---
+
+## ⚙️ Installation
+
+1. **Clone the repository**
+
+   ```bash
+   git clone https://github.com/your-username/skypeek.git
+   cd skypeek
+
+   ```
+
+2. **Navigate to the project folder**
+   ```bash
+   cd skypeek
+   ```
+
+## 📂 Project Structure
+
+```skypeek/
+│── assets/
+│   ├── css/
+│   ├── js/
+│   ├── images/
+│── index.html
+│── README.md
+```
+
+## 👨‍💻 Author
+
+[![Email](https://img.shields.io/badge/Email-atienzajennamiles%40gmail.com-blue?logo=gmail)](mailto:atienzajennamiles@gmail.com)  
+[![Portfolio](https://img.shields.io/badge/Portfolio-binibiningjenna.github.io-brightgreen?logo=google-chrome)](https://binibiningjenna.github.io/)  
+[![GitHub](https://img.shields.io/badge/GitHub-binibiningjenna-black?logo=github)](https://github.com/binibiningjenna)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,272 @@
-AOS.init();
+const info = [
+    { icon: "thermometer-low", category: "Feels like", id: "feelsLike"},
+    { icon: "moisture", category: "Humidity", id: "humidity"},
+    { icon: "wind", category: "Wind", id: "wind"},
+    { icon: "eye", category: "Visibility", id: "visibility"},
+];
 
-am5.ready(function () {
-    // Create root
-    var root = am5.Root.new("hourlyForecastChart");
+let html = '';
+let weatherInfo = document.getElementById('weatherData');
+info.forEach((weather) => {
+    html += `
+     <div class="col-6 text-start">
+       <div class="small-card py-2 ps-3">
+        <div><i class="bi bi-${weather.icon} fs-5 me-2"></i></div>
+        <small>${weather.category}</small>
+        <div class="h5 pt-3" id="${weather.id}"></div>
+       </div>
+    </div>
+    `;
+});
+weatherInfo.innerHTML = html;
 
-    root.setThemes([
-        am5themes_Animated.new(root)
-    ]);
+function showLoader() {
+    document.getElementById('loader').style.display = 'flex';
+    document.getElementById('mainContent').style.display = 'none';
+}
 
-    // Create chart
-    var chart = root.container.children.push(
-        am5xy.XYChart.new(root, {
-            panX: true,
-            panY: false,
-            wheelX: "panX",
-            wheelY: "zoomX",
-            layout: root.verticalLayout
-        })
-    );
+function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'block';
+}
 
-    // Define data
-    var data = [
-        { hour: "1 AM", temp: 24 },
-        { hour: "2 AM", temp: 23 },
-        { hour: "3 AM", temp: 22 },
-        { hour: "4 AM", temp: 22 },
-        { hour: "5 AM", temp: 21 },
-        { hour: "6 AM", temp: 21 },
-        { hour: "7 AM", temp: 22 },
-        { hour: "8 AM", temp: 24 }
-    ];
+const API_KEY = '605b6fec2bba31d4f989397d440c8274';
 
-    // Create X Axis (Category)
-    var xAxis = chart.xAxes.push(
-        am5xy.CategoryAxis.new(root, {
-            categoryField: "hour",
-            renderer: am5xy.AxisRendererX.new(root, {
-                minGridDistance: 40,
-                strokeOpacity: 0.2
-            }),
-            tooltip: am5.Tooltip.new(root, {})
-        })
-    );
+window.onload = function () {
+    useMyLocation();
+};
 
-    xAxis.get("renderer").labels.template.setAll({
-        fill: am5.color(0xf2efeb)
-    });
+function useMyLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            getWeatherByCoords(latitude, longitude);
+        },
+            (error) => {
+                console.warn("Geolocation failed or denied. Loading default city.");
+                getWeatherByCity('Manila');
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 10000,
+                maximumAge: 0
+            }
+        );
+    } else {
+        console.warn("Geolocation not supported. Loading default city.");
+        getWeatherByCity("Manila");
+    }
+}
 
-    xAxis.data.setAll(data);
+async function getWeatherByCity(cityName = null) {
+    try {
+        showLoader();
+        const city = cityName || document.getElementById('searchInput').value;
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        const data = await response.json();
+        displayWeather(data);
+        getFiveDaysForecast(city);
+        getHourlyForecast(city);
+    } catch (error) {
+        console.log(error);
+        showAlert();
+    } finally {
+        hideLoader();
+    }
+}
 
-    // Create Y Axis (Value)
-    var yAxis = chart.yAxes.push(
-        am5xy.ValueAxis.new(root, {
-            renderer: am5xy.AxisRendererY.new(root, {
-                strokeOpacity: 0.2
+async function getWeatherByCoords(lat, lon) {
+    try {
+        showLoader();
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        const data = await response.json();
+        displayWeather(data);
+        getFiveDaysForecast(data.name);
+        getHourlyForecast(data.name);
+    } catch (error) {
+        console.log(error);
+        showAlert();
+    } finally {
+        hideLoader();
+    }
+}
+
+async function getFiveDaysForecast(city) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+        const data = await response.json();
+        const daily = {};
+        data.list.forEach(entry => {
+            const date = entry.dt_txt.split(' ')[0];
+            const hour = entry.dt_txt.split(' ')[1];
+            if (hour === "12:00:00") {
+                daily[date] = entry;
+            }
+        });
+        displayFiveDaysForecast(daily);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function displayFiveDaysForecast(dailyData) {
+    let content = '';
+    let firstDay = true;
+
+    for (let date in dailyData) {
+        let dayName;
+
+        if (firstDay) {
+            dayName = "Today";
+            firstDay = false;
+        } else {
+            dayName = new Date(date).toLocaleDateString('en-US', { weekday: 'short' });
+        }
+        content += `
+        <div class="text-center small-card py-3 px-4 mx-1">
+            <small>${dayName}</small>
+            <div class="h5 py-1 ">${Math.round(dailyData[date].main.temp)}°C</div>
+            <img src="https://openweathermap.org/img/wn/${dailyData[date].weather[0].icon}.png" />
+        </div>
+          `;
+    }
+    document.getElementById('forecast').innerHTML = content;
+}
+
+async function getHourlyForecast(city) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
+        const data = await response.json();
+        const chartData = data.list.slice(0, 8).map(entry => {
+            const date = new Date(entry.dt_txt);
+            const options = { hour: 'numeric', hour12: true };
+            return {
+                hour: date.toLocaleString('en-US', options),
+                temp: Math.round(entry.main.temp)
+            }
+        });
+
+        displayHourlyForecast(chartData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayHourlyForecast(chartData) {
+    am5.ready(function () {
+        // Create root
+        var root = am5.Root.new("hourlyForecastChart");
+
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
+
+        // Create chart
+        var chart = root.container.children.push(
+            am5xy.XYChart.new(root, {
+                panX: true,
+                panY: false,
+                wheelX: "panX",
+                wheelY: "zoomX",
+                layout: root.verticalLayout
             })
-        })
-    );
+        );
 
-    yAxis.get("renderer").labels.template.setAll({
-        fill: am5.color(0xf2efeb)
+        // Create X Axis (Category)
+        var xAxis = chart.xAxes.push(
+            am5xy.CategoryAxis.new(root, {
+                categoryField: "hour",
+                renderer: am5xy.AxisRendererX.new(root, {
+                    minGridDistance: 40,
+                    strokeOpacity: 0.2
+                }),
+                tooltip: am5.Tooltip.new(root, {})
+            })
+        );
+
+        xAxis.get("renderer").labels.template.setAll({
+            fill: am5.color(0xf2efeb)
+        });
+
+        xAxis.data.setAll(chartData);
+
+        // Create Y Axis (Value)
+        var yAxis = chart.yAxes.push(
+            am5xy.ValueAxis.new(root, {
+                renderer: am5xy.AxisRendererY.new(root, {
+                    strokeOpacity: 0.2
+                })
+            })
+        );
+
+        yAxis.get("renderer").labels.template.setAll({
+            fill: am5.color(0xf2efeb)
+        });
+
+        // Create series (Smoothed Line)
+        var series = chart.series.push(
+            am5xy.SmoothedXLineSeries.new(root, {
+                name: "Temperature",
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueYField: "temp",
+                categoryXField: "hour",
+                tooltip: am5.Tooltip.new(root, {
+                    labelText: "{valueY} °C"
+                }),
+                stroke: am5.color(0x00ffff)
+            })
+        );
+
+        series.strokes.template.setAll({
+            strokeWidth: 3
+        });
+
+        series.data.setAll(chartData);
+
+        // Add cursor
+        chart.set("cursor", am5xy.XYCursor.new(root, {
+            behavior: "none",
+            xAxis: xAxis
+        }));
+
+        // Animate
+        series.appear(1000);
+        chart.appear(1000, 100);
     });
+}
 
-    // Create series (Smoothed Line)
-    var series = chart.series.push(
-        am5xy.SmoothedXLineSeries.new(root, {
-            name: "Temperature",
-            xAxis: xAxis,
-            yAxis: yAxis,
-            valueYField: "temp",
-            categoryXField: "hour",
-            tooltip: am5.Tooltip.new(root, {
-                labelText: "{valueY} °C"
-            }),
-            stroke: am5.color(0x00ffff)
-        })
-    );
+function displayWeather(data) {
+    document.getElementById('city').textContent = ` ${data.name}, ${data.sys.country}`;
+    document.getElementById('temp').textContent = `${Math.round(data.main.temp)} °C`;
+    let rawDescription = data.weather[0].description;
+    let capitalizedDescription = rawDescription.charAt(0).toUpperCase() + rawDescription.slice(1);
+    document.getElementById('description').textContent = capitalizedDescription;
+    document.getElementById('feelsLike').textContent = `${Math.round(data.main.feels_like)} °C`;
+    document.getElementById('wind').textContent = `${Math.round(data.wind.speed)} m/s`;
+    document.getElementById('humidity').textContent = `${Math.round(data.main.humidity)} °C`;
+    let visibilityKm = Math.round(data.visibility / 1000);
+    document.getElementById('visibility').textContent = `${visibilityKm} km`;
+}
 
-    series.strokes.template.setAll({
-        strokeWidth: 3
-    });
+function showAlert() {
+    const alert = document.getElementById('alert');
+    const cityName = null || document.getElementById('searchInput').value.trim();
+    document.getElementById('searched-city').textContent = cityName;
+    if (alert) {
+        alert.style.display = 'block';
 
-    series.data.setAll(data);
+        setTimeout(() => {
+            alert.style.display = 'none';
+        }, 8000);
+    }
+}
 
-    // Add cursor
-    chart.set("cursor", am5xy.XYCursor.new(root, {
-        behavior: "none",
-        xAxis: xAxis
-    }));
+document.getElementById('searchInput').addEventListener("keydown", (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        getWeatherByCity();
+    }
+})
 
-    // Animate
-    series.appear(1000);
-    chart.appear(1000, 100);
-}); 
